323ed52b0cb75bbde9a64b39a30d2a2c42aaef14
Nahuel <68520721+ImTheNico@users.noreply.github.com>
2021-08-20 21:57:24 -0300

    Remove repository pattern to this project

--- Core/src/main/java/net/hexaway/board/repository/serialization/ScoreboardModelDeserializer.java
+++
@@ -1,9 +0,0 @@
-package net.hexaway.board.repository.serialization;
-
-import net.hexaway.board.model.ScoreboardModel;
-
-public interface ScoreboardModelDeserializer<T> {
-
-    ScoreboardModel deserializeBoard(T dataType) throws IllegalArgumentException;
-
-}
--- Core/src/main/java/net/hexaway/board/repository/serialization/ScoreboardModelSerializer.java
+++
@@ -1,9 +0,0 @@
-package net.hexaway.board.repository.serialization;
-
-import net.hexaway.board.model.ScoreboardModel;
-
-public interface ScoreboardModelSerializer<T> {
-
-    T serializeBoard(ScoreboardModel scoreboardModel);
-
-}
